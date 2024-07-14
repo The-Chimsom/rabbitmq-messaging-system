14a9409412a8f25013316d7fbd30ecf268532d3c
chimsom <okorogladys52@gmail.com>
2024-07-14 15:23:10 +0100

    fix celery connection

--- messaging_system/app.py
+++ messaging_system/app.py
@@ -2,15 +2,14 @@
 from flask import Flask, request
 import smtplib
 import logging
-from celery import Celery
 from datetime import datetime
+from messaging_system.celery import celery
 
 app = Flask(__name__)
 
 app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
 app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
 
-celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 
 logging.basicConfig(filename='messaging_system.log', level=logging.INFO)
